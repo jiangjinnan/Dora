b9a6ea0f7e54021967f01cba8d06485a27c04d30
TCENT\jjn47239 <jjn47239@ly.com>
2018-12-25 13:39:28 +0800

    Remove source files

--- src/OAuthServer/OAuthServer/IUserInfoAccessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Dora.OAuthServer
-{
-    public interface IUserInfoAccessor<TUser>
-    {
-        Task<TUser> GetUserInfoAsync(UserInfoAccessingContext context);
-    }
-}
