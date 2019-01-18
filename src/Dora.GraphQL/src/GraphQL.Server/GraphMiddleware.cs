b432541683fbcb4bff9fb844054b84c211c7ad6c
jinnan <jinnan@outlook.com>
2019-01-18 19:23:37 +0800

    fix bug

--- src/Dora.GraphQL/src/GraphQL.Server/GraphMiddleware.cs
+++ src/Dora.GraphQL/src/GraphQL.Server/GraphMiddleware.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -14,23 +15,20 @@ namespace Dora.GraphQL.Server
     {
         private readonly RequestDelegate _next;
         private readonly IGraphContextFactory _graphContextFactory;
-        private readonly ISchemaFactory _schemaFactory;
         private readonly IGraphExecutor _executor;
 
-        public GraphMiddleware(RequestDelegate next, IGraphContextFactory graphContextFactory, ISchemaFactory schemaFactory, IGraphExecutor executor)
+        public GraphMiddleware(RequestDelegate next, IGraphContextFactory graphContextFactory, IGraphExecutor executor)
         {
             _next = next ?? throw new ArgumentNullException(nameof(next));
             _graphContextFactory = graphContextFactory ?? throw new ArgumentNullException(nameof(graphContextFactory));
-            _schemaFactory = schemaFactory ?? throw new ArgumentNullException(nameof(schemaFactory));
             _executor = executor ?? throw new ArgumentNullException(nameof(executor));
         }
 
         public async Task InvokeAsync(HttpContext httpContext, IHostingEnvironment hostingEnvironment)
         {
+            var sw = Stopwatch.StartNew();
             var payload = Deserialize<RequestPayload>(httpContext.Request.Body);
             var context = await _graphContextFactory.CreateAsync(payload);
-            var assemblyName = new AssemblyName(hostingEnvironment.ApplicationName);
-            var schema = _schemaFactory.Create(Assembly.Load(assemblyName));
             var result = await _executor.ExecuteAsync(context);
             httpContext.Response.ContentType = "application/json";
             await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(result.Data));
